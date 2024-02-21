77693d68f8a0f2f800dc703761eedff664e8d2c0
etboi1 <et.grundvig@gmail.com>
2024-02-21 16:26:22 -0700

    added the js link to the login page. Wrote the js to store stuff in the local storage and redirect the page. It's not working though, so I'll need to do some debugging

--- js/login.js
+++ js/login.js
@@ -1,3 +1,7 @@
 function login() {
-    
+    const usernameEl = document.querySelector("#username");
+    const passwordEl = document.querySelector("#password");
+    localStorage.setItem("username", usernameEl.value);
+    localStorage.setItem("password", passwordEl.value);
+    window.location.href = "goals.html";
 }
