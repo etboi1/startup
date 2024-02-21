1f95312b034f074f52982ee7742e77da5d27207b
etboi1 <et.grundvig@gmail.com>
2024-02-21 16:57:20 -0700

    added a function to store the data for a new goal in local storage

--- js/newgoal.js
+++ js/newgoal.js
@@ -1,3 +1,15 @@
 function addGoal() {
-    
+    const titleEl = document.querySelector("#goalTitle");
+    localStorage.setItem("goalTitle", titleEl.value);
+    const typeEl = document.querySelector("#goalType");
+    localStorage.setItem("goalTitle", typeEl.value);
+    const descriptionEl = document.querySelector("#goalDescription");
+    localStorage.setItem("goalDescription", descriptionEl.value);
+    const completionDateEl = document.querySelector("#targetCompletionDate");
+    localStorage.setItem("targetCompletionDate", completionDateEl.value);
+    const milestoneDateEl = document.querySelector("#milestoneDate");
+    localStorage.setItem("milestoneDate", milestoneDateEl.value);
+    const milestoneTitleEl = document.querySelector("#milestoneTitle");
+    localStorage.setItem("milestoneTitle", milestoneDateEl.value);
+    window.location.href = "goals.html";
 }
