da752a9bfc11f04bf1c768a16ecc0e7388aab70a
etboi1 <et.grundvig@gmail.com>
2024-02-24 16:25:24 -0700

    changed the format of entering local storage data in an attempt to make an object in the local storage

--- js/newgoal.js
+++ js/newgoal.js
@@ -1,23 +1,17 @@
 function addInfo() {
     const titleEl = document.querySelector("#goalTitle");
-    localStorage.setItem("goalTitle", titleEl.value);
     const typeEl = document.querySelector("#goalType");
-    localStorage.setItem("goalType", typeEl.value);
     const descriptionEl = document.querySelector("#goalDescription");
-    localStorage.setItem("goalDescription", descriptionEl.value);
     const completionDateEl = document.querySelector("#targetCompletionDate");
-    localStorage.setItem("targetCompletionDate", completionDateEl.value);
     const milestoneDateEl = document.querySelector("#milestoneDate");
-    localStorage.setItem("milestoneDate", milestoneDateEl.value);
     const milestoneTitleEl = document.querySelector("#milestoneTitle");
-    localStorage.setItem("milestoneTitle", milestoneTitleEl.value);
+    localStorage.setItem(typeEl.value, 
+        {
+            "goalTitle": titleEl.value,
+            "goalDescription": descriptionEl.value,
+            "targetCompletionDate": completionDateEl.value,
+            "milestoneDate": milestoneDateEl.value,
+            "milestoneTitle": milestoneTitleEl.value,
+        });
     window.location.href = "goals.html";
-}
-
-function addGoal() {
-
-}
-
-function populateGoal(element) {
-    
 }
