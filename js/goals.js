c74b4826d12c353f891e3802b8ccfb3329753ca9
etboi1 <et.grundvig@gmail.com>
2024-02-25 12:39:15 -0700

    mostly debugging stuff on the goals.js page

--- js/goals.js
+++ js/goals.js
@@ -1,36 +1,64 @@
 function onInit() {
-    let key = {0: 'physical', 1: 'educational', 2: 'occupational', 3: 'hobbies', 4: 'social'};
-    let goals = localStorage.getItem('goals');
-    goals = JSON.parse(goals);
-    for (let i = 0; i < goals.length - 1; i++) {
-        let goalType = key[i];
-        if (goals[i].length > 0) {
-            let newGoal = goals[i][goals[i].length - 1]
-            let goalTitle = newGoal.goalTitle
-            let dueDate = newGoal.targetCompletionDate
-            let milestoneDate = newGoal.milestoneDate
-            let milestoneTitle = newGoal.milestoneTitle
+    let allGoals = localStorage.getItem('goals');
+    allGoals = JSON.parse(allGoals);
+    for ([goalType, specificGoals] of Object.entries(allGoals)) {
+        let id = `${goalType}Goals`
+        console.log(id);
+        goalOuterContainerEl = document.getElementById(id);
+        if (goalOuterContainerEl.children.length < allGoals[goalType].length) {
+            //Retrieve the info of the new goal
+            let newGoal = allGoals[goalType][allGoals[goalType].length - 1];
+            let goalTitle = newGoal.goalTitle;
+            let dueDate = newGoal.targetCompletionDate;
+            let milestoneDate = newGoal.milestoneDate;
+            let milestoneTitle = newGoal.milestoneTitle;
 
-            goalOuterContainerEl = document.getElementById(`#${goalType}Goals`);
-            if (goalOuterContainerEl.getChildCount() < goals.at(i).length()) {
-                // initialize all the new HTML elements
-                let firstChild = document.createElement('div');
-                firstChild.classList.add("inner-container");
-                let secondChild1 = document.createElement('strong');
-                secondChild1.innerHTML = `${goalTitle}`;
-                let secondChild2 = document.createElement('span');
-                secondChild2.innerHTML = 'Due Date - ' + `${dueDate}`;
-                let secondChild3 = document.createElement('span');
-                secondChild3.innerHTML = `${milestoneTitle}` + ' - ' `${milestoneDate}`;
+            // initialize all the new HTML elements
+            let firstChild = document.createElement('div');
+            firstChild.classList.add("inner-container");
+            let secondChild1 = document.createElement('strong');
+            secondChild1.innerHTML = `${goalTitle}`;
+            let secondChild2 = document.createElement('span');
+            secondChild2.innerHTML = `Due Date - ${dueDate}`;
+            let secondChild3 = document.createElement('span');
+            secondChild3.innerHTML = `${milestoneTitle} - ${milestoneDate}`;
 
-                // actually add them to the correct parent elements
-                goalOuterContainerEl.appendChild(firstChild);
-                firstChild.appendChild(secondChild1);
-                firstChild.appendChild(secondChild2);
-                firstChild.appendChild(secondChild3);
-            }
+            // actually add them to the correct parent elements
+            goalOuterContainerEl.appendChild(firstChild);
+            firstChild.appendChild(secondChild1);
+            firstChild.appendChild(secondChild2);
+            firstChild.appendChild(secondChild3);
         }
     }
+    // for (let i = 0; i < goals.length - 1; i++) {
+    //     let goalType = key[i];
+    //     if (goals[i].length > 0) {
+    //         let newGoal = goals[i][goals[i].length - 1];
+    //         let goalTitle = newGoal.goalTitle;
+    //         let dueDate = newGoal.targetCompletionDate;
+    //         let milestoneDate = newGoal.milestoneDate;
+    //         let milestoneTitle = newGoal.milestoneTitle;
+
+    //         goalOuterContainerEl = document.getElementById(`#${goalType}Goals`);
+    //         if (goalOuterContainerEl.getChildCount() < goals.at(i).length()) {
+    //             // initialize all the new HTML elements
+    //             let firstChild = document.createElement('div');
+    //             firstChild.classList.add("inner-container");
+    //             let secondChild1 = document.createElement('strong');
+    //             secondChild1.innerHTML = `${goalTitle}`;
+    //             let secondChild2 = document.createElement('span');
+    //             secondChild2.innerHTML = 'Due Date - ' + `${dueDate}`;
+    //             let secondChild3 = document.createElement('span');
+    //             secondChild3.innerHTML = `${milestoneTitle}` + ' - ' `${milestoneDate}`;
+
+    //             // actually add them to the correct parent elements
+    //             goalOuterContainerEl.appendChild(firstChild);
+    //             firstChild.appendChild(secondChild1);
+    //             firstChild.appendChild(secondChild2);
+    //             firstChild.appendChild(secondChild3);
+    //         }
+    //     }
+    // }
 }
 
 onInit()
--- js/newgoal.js
+++ js/newgoal.js
@@ -1,12 +1,8 @@
 function saveGoal() {
-
-}
-
-function saveGoalHelper(goalType) {
-    let `${goalType}` = [[],[],[],[],[]];
-    const goalsText = localStorage.getItem(`${goalType}`);
+    let goals = {'Physical':[], 'Educational':[], 'Occupational':[], 'Hobbies':[], 'Social':[]};
+    const goalsText = localStorage.getItem('goals');
     if (goalsText) {
-        `${goalType}` = JSON.parse(goalsText);
+        goals = JSON.parse(goalsText);
     }
 
     const titleEl = document.querySelector("#goalTitle");
@@ -16,35 +12,22 @@ function saveGoalHelper(goalType) {
     const milestoneDateEl = document.querySelector("#milestoneDate");
     const milestoneTitleEl = document.querySelector("#milestoneTitle");
 
-    `${goalType}` = this.updateGoals(goals, typeEl, titleEl, descriptionEl, completionDateEl, milestoneDateEl, milestoneTitleEl);
+    goals = this.updateGoals(goals, typeEl, titleEl, descriptionEl, completionDateEl, milestoneDateEl, milestoneTitleEl);
 
-    localStorage.setItem('goals', JSON.stringify(`${goalType}`);
-    
+    localStorage.setItem('goals', JSON.stringify(goals));
+   
     window.location.href = "goals.html";
 }
 
 function updateGoals(goals, type, title, description, completionDate, milestoneDate, milestoneTitle) {
-    const newGoal = {"goalTitle": title.value, 
+    const newGoal = {"goalTitle": title.value,
         "goalDescription": description.value,
         "targetCompletionDate": completionDate.value,
         "milestoneDate": milestoneDate.value,
         "milestoneTitle": milestoneTitle.value}
-    let ind = 0;
 
-    if (type.value == 'Educational') {
-        ind = 1;
-    }
-    else if (type.value == 'Occupational') {
-        ind = 2;
-    }
-    else if (type.value == 'Hobbies') {
-        ind = 3;
-    }
-    else if (type.value == 'Social') {
-        ind = 4;
-    }
-
-    goals[ind].push(newGoal);
+    goalType = type.value;
+    goals[goalType].push(newGoal);
 
     return goals;
 }
