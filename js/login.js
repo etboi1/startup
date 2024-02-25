eb6802904864e92f043dacf8048d1af725ac3648
etboi1 <et.grundvig@gmail.com>
2024-02-24 17:39:50 -0700

    added functionality to add JSON to the local storage when making a new goal

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 function login() {
     const usernameEl = document.querySelector("#username");
     const passwordEl = document.querySelector("#password");
-    localStorage.setItem("username", usernameEl.value);
+    localStorage.setItem("userName", usernameEl.value);
     localStorage.setItem("password", passwordEl.value);
     window.location.href = "goals.html";
 }
--- js/newgoal.js
+++ js/newgoal.js
@@ -1,17 +1,50 @@
-function addInfo() {
+function getGoalInfo() {
     const titleEl = document.querySelector("#goalTitle");
     const typeEl = document.querySelector("#goalType");
     const descriptionEl = document.querySelector("#goalDescription");
     const completionDateEl = document.querySelector("#targetCompletionDate");
     const milestoneDateEl = document.querySelector("#milestoneDate");
     const milestoneTitleEl = document.querySelector("#milestoneTitle");
-    localStorage.setItem(typeEl.value, 
-        {
-            "goalTitle": titleEl.value,
-            "goalDescription": descriptionEl.value,
-            "targetCompletionDate": completionDateEl.value,
-            "milestoneDate": milestoneDateEl.value,
-            "milestoneTitle": milestoneTitleEl.value,
-        });
+    return [typeEl, titleEl, descriptionEl, completionDateEl, milestoneDateEl, milestoneTitleEl]
+}
+
+function saveGoal() {
+    let goals = [[],[],[],[],[]];
+    const goalsText = localStorage.getItem('goals');
+    if (goalsText) {
+        goals = JSON.parse(goalsText);
+    }
+
+    goalAttributes = getGoalInfo;
+    goals = this.updateGoals(goalAttributes);
+
+    localStorage.setItem('goals', JSON.stringify(goals));
+    
     window.location.href = "goals.html";
+}
+
+function updateGoals(goalData) {
+    const newGoal = {"goalTitle": goalData[1].value, 
+        "goalDescription": goalData[2].value,
+        "targetCompletionDate": goalAttributes[3].value,
+        "milestoneDate": goalAttributes[4].value,
+        "milestoneTitle": goalAttributes[5].value}
+    let ind = 0;
+
+    if (goalData[0].value == 'Educational') {
+        ind = 1;
+    }
+    else if (goalData[0].value == 'Occupational') {
+        ind = 2;
+    }
+    else if (goalData[0].value == 'Hobbies') {
+        ind = 3;
+    }
+    else if (goalData[0].value == 'Social') {
+        ind = 4;
+    }
+
+    goals[ind].push(newGoal);
+
+    return goals;
 }
