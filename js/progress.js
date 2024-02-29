a5635007b9982fa094a404aa882d75858849d203
etboi1 <et.grundvig@gmail.com>
2024-02-28 17:28:31 -0700

    added a function to correctly render the appropriate information according to the right goal

--- js/progress.js
+++ js/progress.js
@@ -1,3 +1,15 @@
-function updateProgress () {
-    
+function updateProgress() {
+    window.location.href = "goals.html";
 }
+
+function onProgressInit() {
+    const milestoneTitle = localStorage.getItem('milestoneTitle');
+    const milestoneDate = localStorage.getItem('milestoneDate');
+
+    const mainEl = document.querySelector('main');
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = `Your target completion date for the milestone "${milestoneTitle}" is ${milestoneDate}. Report status below:`;
+    mainEl.insertBefore(newParagraph, mainEl.firstChild);
+}
+
+onProgressInit()
