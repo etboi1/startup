3bf58a85d2d079ba7d86c1885535c534d03b8a44
etboi1 <et.grundvig@gmail.com>
2024-03-06 14:24:51 -0700

    put stuff back in its correct folder

--- js/login.js
+++
@@ -1,7 +0,0 @@
-function login() {
-    const usernameEl = document.querySelector("#username");
-    const passwordEl = document.querySelector("#password");
-    localStorage.setItem("username", usernameEl.value);
-    localStorage.setItem("password", passwordEl.value);
-    window.location.href = "goals.html";
-}
