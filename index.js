20d39104ce3b23bb5344bfee358761ef45e1af3a
etboi1 <et.grundvig@gmail.com>
2024-03-06 13:39:35 -0700

    started creating the endpoints for my service. express.Router to do so

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const app = express();
 
+//sets default service port to 3000 unless another one is provided
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+//include the JSON body parsing middleware - goal data is passed in using JSON
+app.use(express.json());
+
+//just like simon, serve up front-end static content from public directory
+app.use(express.static('public'));
+
+//I'm gonna use apiRouter for service endpoints, just like simon
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+//Get Goals
+
+//Submit New Goals
+
+//Get Shared Goals
+
+//Share New Goal
