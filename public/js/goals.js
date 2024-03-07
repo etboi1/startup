3f57d99b7b946753627590c8c7ada038a90907cf
etboi1 <et.grundvig@gmail.com>
2024-03-06 21:35:36 -0700

    added the code to try and retrieve the data from the backend for the goals, and if not to load it from local storage

--- public/js/goals.js
+++ public/js/goals.js
@@ -1,48 +1,57 @@
-function onInit() {
+async function onInit() {
     let username = localStorage.getItem('username');
     const usernameEl = document.querySelector('h8');
     usernameEl.textContent = `Welcome ${username}!`;
+
+    let allGoals = {'Physical':[], 'Educational':[], 'Occupational':[], 'Hobbies':[], 'Social':[]};
+
+    try {
+        const response = await fetch('/api/goals');
+        allGoals = await response.json();
+    }
+    catch {
+        if (localStorage.getItem('goals')) {
+            allGoals = localStorage.getItem('goals');
+            allGoals = JSON.parse(allGoals);
+        }
+    }
     
-    if (localStorage.getItem('goals')) {
-        let allGoals = localStorage.getItem('goals');
-        allGoals = JSON.parse(allGoals);
-        for ([goalType, specificGoals] of Object.entries(allGoals)) {
-            let id = `${goalType}Goals`
-            console.log(id);
-            goalOuterContainerEl = document.getElementById(id);
-            if (goalOuterContainerEl.children.length < allGoals[goalType].length) {
-                for (let i = 0; i < allGoals[goalType].length; i++) {
-                    //Retrieve the info of the new goal
-                    let newGoal = allGoals[goalType][i];
-                    let goalTitle = newGoal.goalTitle;
-                    let dueDate = newGoal.targetCompletionDate;
-                    let milestoneDate = newGoal.milestoneDate;
-                    let milestoneTitle = newGoal.milestoneTitle;
+    for ([goalType, specificGoals] of Object.entries(allGoals)) {
+        let id = `${goalType}Goals`
+        console.log(id);
+        goalOuterContainerEl = document.getElementById(id);
+        if (goalOuterContainerEl.children.length < allGoals[goalType].length) {
+            for (let i = 0; i < allGoals[goalType].length; i++) {
+                //Retrieve the info of the new goal
+                let newGoal = allGoals[goalType][i];
+                let goalTitle = newGoal.goalTitle;
+                let dueDate = newGoal.targetCompletionDate;
+                let milestoneDate = newGoal.milestoneDate;
+                let milestoneTitle = newGoal.milestoneTitle;
 
-                    // initialize all the new HTML elements
-                    let firstChild = document.createElement('div');
-                    firstChild.classList.add("inner-container");
-                    let secondChild1 = document.createElement('strong');
-                    secondChild1.innerHTML = `${goalTitle}`;
-                    let secondChild2 = document.createElement('span');
-                    secondChild2.innerHTML = `Due Date - ${dueDate}`;
-                    let secondChild3 = document.createElement('span');
-                    secondChild3.innerHTML = `${milestoneTitle} - ${milestoneDate}`;
-                    let secondChild4 = document.createElement('div');
-                    secondChild4.addEventListener('click', sendToProgress);
-                    let thirdChild = document.createElement('button');
-                    thirdChild.classList.add("button");
-                    thirdChild.type = "submit";
-                    thirdChild.innerHTML = 'Report Progress'
+                // initialize all the new HTML elements
+                let firstChild = document.createElement('div');
+                firstChild.classList.add("inner-container");
+                let secondChild1 = document.createElement('strong');
+                secondChild1.innerHTML = `${goalTitle}`;
+                let secondChild2 = document.createElement('span');
+                secondChild2.innerHTML = `Due Date - ${dueDate}`;
+                let secondChild3 = document.createElement('span');
+                secondChild3.innerHTML = `${milestoneTitle} - ${milestoneDate}`;
+                let secondChild4 = document.createElement('div');
+                secondChild4.addEventListener('click', sendToProgress);
+                let thirdChild = document.createElement('button');
+                thirdChild.classList.add("button");
+                thirdChild.type = "submit";
+                thirdChild.innerHTML = 'Report Progress'
 
-                    // actually add them to the correct parent elements
-                    goalOuterContainerEl.appendChild(firstChild);
-                    firstChild.appendChild(secondChild1);
-                    firstChild.appendChild(secondChild2);
-                    firstChild.appendChild(secondChild3);
-                    firstChild.appendChild(secondChild4);
-                    secondChild4.appendChild(thirdChild);
-                }
+                // actually add them to the correct parent elements
+                goalOuterContainerEl.appendChild(firstChild);
+                firstChild.appendChild(secondChild1);
+                firstChild.appendChild(secondChild2);
+                firstChild.appendChild(secondChild3);
+                firstChild.appendChild(secondChild4);
+                secondChild4.appendChild(thirdChild);
             }
         }
     }
