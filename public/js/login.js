5e794c7d5073436a614d76135313bf37b33ec79a
etboi1 <et.grundvig@gmail.com>
2024-03-21 17:12:59 -0600

    added the code on login.js, index.html, and login.css to allow for creating a new user or logging in as an existing one. Still don't know how to display the error message

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,47 @@
-async function login() {
-    const usernameEl = document.querySelector("#username");
-    const passwordEl = document.querySelector("#password");
-
-    try {
-        const response = await fetch(`/api/user`, {
-            method: 'POST',
-            headers: {'content-type': 'application/json'},
-            body: JSON.stringify([usernameEl.value, passwordEl.value]),
-        });
-        let usernameObject = await response.json();
-        let username = usernameObject.currentUser;
-        localStorage.setItem('currentUser', username);
-        localStorage.setItem('password', passwordEl.value);
-    }
-    catch {
-        localStorage.setItem('currentUser', usernameEl.value);
-        localStorage.setItem('password', passwordEl.value)
-    }
-    window.location.href = "goals.html";
+async function loginUser() {
+    loginOrCreate('/api/auth/create');
 }
+
+async function createUser() {
+    loginOrCreate('api/auth/create');
+}
+
+async function loginOrCreate(endpoint) {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const response = await fetch(endpoint, {
+        method: 'POST',
+        body: JSON.stringify({ username: username, password:password}),
+        headers: {'Content-type': 'application/json; charset=UTF-8'},
+    });
+
+    if (response.ok) {
+        localStorage.setItem('username', username);
+        window.location.href = 'goals.html';
+    }
+    else {
+        const body = await response.json();
+    }
+}
+
+// async function login() {
+//     const usernameEl = document.querySelector("#username");
+//     const passwordEl = document.querySelector("#password");
+
+//     try {
+//         const response = await fetch(`/api/user`, {
+//             method: 'POST',
+//             headers: {'content-type': 'application/json'},
+//             body: JSON.stringify([usernameEl.value, passwordEl.value]),
+//         });
+//         let usernameObject = await response.json();
+//         let username = usernameObject.currentUser;
+//         localStorage.setItem('currentUser', username);
+//         localStorage.setItem('password', passwordEl.value);
+//     }
+//     catch {
+//         localStorage.setItem('currentUser', usernameEl.value);
+//         localStorage.setItem('password', passwordEl.value)
+//     }
+//     window.location.href = "goals.html";
+// }
