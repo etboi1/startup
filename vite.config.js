839343af7fd8e51b75c3b0bff6ce292a6f69bf1c
etboi1 <et.grundvig@gmail.com>
2024-04-11 15:44:23 -0600

    fixed the errors in the config file that was messing up the debugging

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
-        notify: {
-            '/api': 'http//localhost:3000',
+        proxy: {
+            '/api': 'http://localhost:3000',
             '/ws': {
                 target: 'ws://localhost:3000',
                 ws:true,
