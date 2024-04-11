7c6774e697bfe04168b60ba43ca926a6af25a12a
etboi1 <et.grundvig@gmail.com>
2024-04-11 16:36:58 -0600

    made it so the accordions correctly rendered on the personal goals page

--- src/goals/goals.jsx
+++ src/goals/goals.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
+import Accordion from 'react-bootstrap/Accordion';
+import '../styles.css';
+
 export function Goals() {
     return (
-        <div></div>
+        <main>
+            <h1 className='fix-header'>Personal Goals</h1>
+            <h4 className='center-text' id='inspirationalQuote'></h4>
+            <Accordion defaultActiveKey="0" className='accordion'>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Physical Goals</Accordion.Header>
+                    <Accordion.Body>Example Goal</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='1'>
+                    <Accordion.Header>Educational Goals</Accordion.Header>
+                    <Accordion.Body>Example Goal</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Occupational Goals</Accordion.Header>
+                    <Accordion.Body>Example Goal</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='3'>
+                    <Accordion.Header>Hobby Goals</Accordion.Header>
+                    <Accordion.Body>Example Goal</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='4'>
+                    <Accordion.Header>Social Goals</Accordion.Header>
+                    <Accordion.Body>Example Goal</Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </main>
     )
 }
