db1f8c6d27725b9fac518d23ed481c377fcef6d3
etboi1 <et.grundvig@gmail.com>
2024-04-11 18:02:22 -0600

    fixed something that was messing up the logout function

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -13,7 +13,6 @@ export function Authenticated(props) {
         .finally(() => {
             localStorage.removeItem('username');
             props.onLogout();
-            Reroute('/');
         })
     }
 
